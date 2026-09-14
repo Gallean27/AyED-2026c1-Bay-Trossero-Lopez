1f7f73b41ed0299b0df917e94feee50667ee33f9
Mateo Trossero <mateo123trossero321@hotmail.com>
2026-09-14 19:16:02 -0300

    Corregido el import de modulo1 a lde

--- TrabajoPractico_1/proyecto_1/main.py
+++ TrabajoPractico_1/proyecto_1/main.py
@@ -1,13 +1,17 @@
-import modulo1
-
+from modules.lde import ListaDobleEnlazada
 
 def main():
-    #prueba de metodos
-    lista = modulo1.ListaDobleEnlazada()
-
+    # prueba de metodos
+    lista = ListaDobleEnlazada()
+    
     lista.agregar_al_inicio(1)
     lista.agregar_al_inicio(2)
     lista.agregar_al_inicio(3)
-    lista.agregar_al_final(4)
-    lista.agregar_al_final(5)
-    lista.agregar_al_final(6)
+    lista.agregar_al_inicio(4)
+    lista.agregar_al_inicio(5)
+    lista.agregar_al_inicio(6)
+    
+    print("¡Felicidades! Los datos se cargaron en la lista correctamente.")
+
+if __name__ == "__main__":
+    main()
